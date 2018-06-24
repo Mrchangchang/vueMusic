f6819ab58fbd54299025f2620df5420f1f808845
mrchangchang <78172670@qq.com>
2018-06-24 10:52:14 +0800

    推荐页面开发

---
+++ src/api/config.js
@@ -0,0 +1,18 @@
+/**
+ * Created by chang on 2018/6/15.
+ */
+export const commonParams = {
+  g_tk: 5381,
+  format: 'json',
+  inCharset: 'utf-8',
+  outCharset: 'utf-8',
+  notice: 0,
+  needNewCode: 1,
+  _: 1528994169428
+}
+
+export const options = {
+  param: 'jsonpCallback'
+}
+
+export const ERR_OK = 0
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,7 +1,120 @@
 <template>
-    <div>recommend</div>
+  <div class="recommend" ref="recommend">
+    <scroll class="recommend-content">
+      <div>
+        <div v-if="recommends.length" class="slider-wrapper" ref="sliderWrapper" :data="discList">
+          <slider>
+            <div v-for="item in recommends">
+              <a :href="item.linkUrl">
+                <img class="needsclick" :src="item.picUrl">
+              </a>
+            </div>
+          </slider>
+        </div>
+        <div class="recommend-list">
+          <h1 class="list-title">热门歌单推荐</h1>
+          <ul>
+            <li @click="selectItem(item)" v-for="item in discList" class="item">
+              <div class="icon">
+                <img width="60" height="60" v-lazy="item.picUrl">
+              </div>
+              <div class="text">
+                <h2 class="name" v-html="item.songListAuthor"></h2>
+                <p class="desc" v-html="item.songListDesc"></p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+    </scroll>
+    <div class="loading-container" v-show="!discList.length">
+      <loading></loading>
+    </div>
+    <router-view></router-view>
+  </div>
 </template>
 <script>
-export default {}
+import Scroll from 'base/scroll/scroll'
+import Slider from 'base/slider/slider'
+import Loading from 'base/loading/loading'
+import { getRecommend } from '../../api/recommend'
+import { ERR_OK } from '../../api/config'
+
+export default {
+  data () {
+    return {
+      recommends: [],
+      discList: []
+    }
+  },
+  created () {
+    this._getRecommend()
+  },
+  methods: {
+    _getRecommend () {
+      getRecommend().then(res => {
+        if (res.code === ERR_OK) {
+          this.recommends = res.data.slider
+          this.discList = res.data.songList
+        }
+      })
+    }
+  },
+  components: {
+    Slider,
+    Scroll,
+    Loading
+  }
+}
 </script>
-<style scoped lang="stylus" rel="stylesheet/stylus"></style>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+
+  .recommend
+    position: fixed
+    width: 100%
+    top: 88px
+    bottom: 0
+    .recommend-content
+      height: 100%
+      overflow: hidden
+      .slider-wrapper
+        position: relative
+        width: 100%
+        overflow: hidden
+      .recommend-list
+        .list-title
+          height: 65px
+          line-height: 65px
+          text-align: center
+          font-size: $font-size-medium
+          color: $color-theme
+        .item
+          display: flex
+          box-sizing: border-box
+          align-items: center
+          padding: 0 20px 20px 20px
+          .icon
+            flex: 0 0 60px
+            width: 60px
+            padding-right: 20px
+          .text
+            display: flex
+            flex-direction: column
+            justify-content: center
+            flex: 1
+            line-height: 20px
+            overflow: hidden
+            font-size: $font-size-medium
+            .name
+              margin-bottom: 10px
+              color: $color-text
+            .desc
+              color: $color-text-d
+      .loading-container
+        position: absolute
+        width: 100%
+        top: 50%
+        transform: translateY(-50%)
+</style>
